03f18a445eb19ace599c592b0ba9cb1c57e2dc8c
Josh <joshconstantinelim@gmail.com>
2025-07-14 15:08:51 +1000

    wk7

--- wk7/findShortestDist.c
+++ wk7/findShortestDist.c
@@ -1,3 +1,4 @@
+#include <limits.h>
 #include <stdbool.h>
 #include <stdio.h>
 #include <stdlib.h>
@@ -9,6 +10,30 @@
 // distance between src and each vertex.
 // For an undirected, unweighted graph.
 int *findShortestDistance(Graph g, int src) {
-    // TODO
-    return 0;
+    Queue q = QueueNew();
+    bool *visited = calloc(GraphNumVertices(g), sizeof(bool));
+    int *dist = malloc(GraphNumVertices(g) * sizeof(int));
+    for (int i = 0; i < GraphNumVertices(g); i++) {
+        dist[i] = INT_MAX;
+    }
+
+    QueueEnqueue(q, src);
+    visited[src] = true;
+    dist[src] = 0;
+
+    while (!QueueIsEmpty(q)) {
+        int v = QueueDequeue(q);
+        for (int w = 0; w < GraphNumVertices(g); w++) {
+            if (GraphIsAdjacent(g, v, w) && !visited[w]) {
+                QueueEnqueue(q, w);
+                visited[w] = true;
+                dist[w] = dist[v] + 1;
+            }
+        }
+    }
+
+    free(visited);
+    QueueFree(q);
+
+    return dist;
 }
