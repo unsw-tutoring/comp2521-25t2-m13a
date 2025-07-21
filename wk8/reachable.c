6dc742221391d9e546d30de0247bf81b3eb7867a
Josh <joshconstantinelim@gmail.com>
2025-07-21 14:05:31 +1000

    wk8

--- wk8/reachable.c
+++ wk8/reachable.c
@@ -5,7 +5,18 @@
 #include "Graph.h"
 #include "Set.h"
 
+void doReachable(Graph g, Vertex curr, Set visited) {
+    if (SetContains(visited, curr)) return;
+
+    SetInsert(visited, curr);
+
+    for (Vertex next = 0; next < GraphNumVertices(g); next++) {
+        if (GraphIsAdjacent(g, curr, next)) doReachable(g, next, visited);
+    }
+}
+
 Set reachable(Graph g, Vertex src) {
-    // TODO
-    return NULL;
+    Set visited = SetNew();
+    doReachable(g, src, visited);
+    return visited;
 }
